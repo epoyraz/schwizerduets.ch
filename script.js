0f3c27b778fff62edea33575497469f4cfa4eb0b
Enes Poyraz <epoyraz.eth@googlemail.com>
2023-06-27 12:11:50 +0200

    add sample text functionality

--- script.js
+++ script.js
@@ -2,14 +2,45 @@ document.addEventListener("DOMContentLoaded", (event) => {
   document
     .getElementById("myTextarea")
     .addEventListener("input", updateCharactersLeft);
-  document.getElementById("myForm").addEventListener("submit", submitForm);
+  document.getElementById("submitButton").addEventListener("click", submitForm);
+
+  document
+    .getElementById("sampleText1")
+    .addEventListener("click", addSampleText);
+  document
+    .getElementById("sampleText2")
+    .addEventListener("click", addSampleText);
+  document
+    .getElementById("sampleText3")
+    .addEventListener("click", addSampleText);
+  document
+    .getElementById("sampleText4")
+    .addEventListener("click", addSampleText);
 });
+
+function addSampleText(event) {
+  // check id of event target id
+  var sampleText = "";
+  if (event.target.id === "sampleText1") {
+    sampleText = "Ich bin ein Schwizerdütsch Text";
+  }
+  if (event.target.id === "sampleText2") {
+    sampleText =
+      "Das Netz ist unser Kernstück. Damit versorgen wir die Schweiz mit besonders leistungsfähigen und superschnellen Breitbandtechnologien. Und unser Netzausbau geht weiter: Bis Ende 2025 bauen wir die Glasfasernetz-Abdeckung in der Schweiz auf zwischen 50 und 55 Prozent aus und treiben parallel dazu den Mobilfunkausbau voran. Mit jährlichen Investitionen von 1,6 Mrd. Franken wollen wir unseren Kundinnen und Kunden eine herausragende Infrastruktur anbieten und leisten damit auch einen Beitrag zu einer wettbewerbsfähigen Schweiz.";
+  }
+  if (event.target.id === "sampleText3") {
+    sampleText = "Ich bin ein dritter Schwizerdütsch Text";
+  }
+  if (event.target.id === "sampleText4") {
+    sampleText = "";
+  }
+  document.getElementById("myTextarea").value = sampleText;
+}
 
 function updateCharactersLeft() {
   var inputLength = document.getElementById("myTextarea").value.length;
   var charsLeft = 300 - inputLength;
-  document.getElementById("charsLeft").innerText =
-    charsLeft + " Zeichen übrig";
+  document.getElementById("charsLeft").innerText = charsLeft + " Zeichen übrig";
 }
 
 async function submitForm(event) {
